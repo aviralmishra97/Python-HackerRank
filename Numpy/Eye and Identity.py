9690ead7d466601827be9821732df77e46ee9098
Aviral Mishra <117946353+aviralmishra97@users.noreply.github.com>
2022-11-18 15:54:13 +0530

    Update Eye and Identity
    In the previous code there was no space given in between the array element so test case were failing the new code is error free

--- Numpy/Eye and Identity.py
+++ Numpy/Eye and Identity.py
@@ -1,3 +1,7 @@
 import numpy
 dimensions = tuple(map(int, input().split()))
 print(numpy.eye(dimensions[0], dimensions[1], k=0))
+
+or 
+import numpy
+print(str(numpy.eye(*map(int,input().split()))).replace('1',' 1').replace('0',' 0'))
